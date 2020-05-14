2400f2235aee54167260051180bedba2fc816995
Dxoca <987284242@qq.com>
2020-05-14 16:46:12 +0800

    修改逻辑结构

--- admin.php
+++ admin.php
@@ -1,26 +1,23 @@
 <?php
 require_once 'model.php';//加载模块
 header('Content-type:text/html; charset=utf-8');
-
 $mysqli = connect();//连接数据库
-
-$nowTime = getNowTime();//获取 登录时间 方便写入登录 login
-$email = $_POST['email'];
-$password = $_POST['password'];
-
-$result = $mysqli->query("select * from `user` where email='$email'");//从user表中查询登录信息符合的字段  and password='$password'
-$mysqli = null;//清理缓存
-//print_r($result);
-$row = $result->fetch_row();//取出email项
-print_r($row);//登录数据
 ?>
 
 <?php
 //session 开始
 session_start();
 //处理登录信息 从登录界面进入
-
 if (isset($_POST['login'])) {
+    $nowTime = getNowTime();//获取 登录时间 方便写入登录 login
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $result = $mysqli->query("select * from `user` where email='$email'");//从user表中查询登录信息符合的字段  and password='$password'
+    $mysqli = null;//清理缓存
+    //print_r($result);
+    $row = $result->fetch_row();//取出email项
+
     # 接受登录信息
     # 判断登录信息
     if (($email == '') || ($password == '')) {
@@ -28,7 +25,7 @@ if (isset($_POST['login'])) {
         header('refresh:1; url=login.php');
         echo "<script>alert('用户名或密码不能为空,系统将在3秒后跳转到登录界面,请重新填写登录信息!')</script>";
         exit;
-    }elseif  (($email != $row[0]) || ($password != $row[2])) {
+    } elseif (($email != $row[0]) || ($password != $row[2])) {
         # 用户名或密码错误,同空的处理方式
         header('refresh:1; url=login.php');
         echo "<script>alert('用户名或密码错误,系统将在3秒后跳转到登录界面,请重新填写登录信息!')</script>";
@@ -37,25 +34,25 @@ if (isset($_POST['login'])) {
         # 用户名和密码都正确,将用户信息存到Session中
         $_SESSION['email'] = $email;
         $_SESSION['isLogin'] = 1;//1 已登录
-        $_SESSION['username']=$row[1];//用户名
+        $_SESSION['username'] = $row[1];//用户名
         // 若勾选7天内自动登录,则将其保存到Cookie并设置保留7天
         if ($_POST['remember'] == "yes") {
-            setcookie('email', $email, time()+7*24*60*60);
-            setcookie('code', md5($email.md5($password)), time()+7*24*60*60);
+            setcookie('email', $email, time() + 7 * 24 * 60 * 60);
+            setcookie('code', md5($email . md5($password)), time() + 7 * 24 * 60 * 60);
         } else {
             // 没有勾选则删除Cookie
-            setcookie('email', '', time()-999);
-            setcookie('code', '', time()-999);
+            setcookie('email', '', time() - 999);
+            setcookie('code', '', time() - 999);
         }
         // 处理完附加项后跳转到登录成功的首页
         echo "<script>alert('用户 $row[1] 登录成功！$nowTime')</script>";//显示用户名 并提示登录成功
 //        header('location:login.php');
     }
-}else{
-    if(isLogin()){
+} else {//直接进入admin页面
+    if (isLogin()) {
         echo "当前已登录";
         echo "后台管理 ";
-    }else{
+    } else {
         header('refresh:0.1; url=login.php');
         echo "<script>alert('登录失败，请重新登录')</script>";
     }
